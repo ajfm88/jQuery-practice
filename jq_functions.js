34a640cd411842593199d7d4a318214edaed9313
unknown <ajfoucaultmo@mail.usf.edu>
2022-06-19 21:06:16 -0700

    52. jQuery Events Intro

--- jq_functions.js
+++ jq_functions.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-  $('button').click(function () {
-    // $('*').hide(); hides all elements
-    // $(this).hide(); selects current html element
-    $(':button').hide(); // selects all button elements
+  $('p').dblclick(function () {
+    $(this).hide();
   });
 });
