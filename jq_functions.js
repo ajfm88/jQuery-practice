d952a2a51a76a68d52928f1963438478829db238
unknown <ajfoucaultmo@mail.usf.edu>
2022-06-20 19:57:37 -0700

    57. jQuery Toggle

--- jq_functions.js
+++ jq_functions.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
-  $('#hidden').click(function () {
-    $('p').hide(1000);
-  });
-  $('#visible').click(function () {
-    $('p').show(1000);
+  $('button').click(function () {
+    $('p').toggle(1000);
   });
 });
